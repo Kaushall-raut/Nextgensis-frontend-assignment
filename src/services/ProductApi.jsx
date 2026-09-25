a2babbb607df3807a730b490aed56c9da77931bf
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-09-25 09:15:27 +0530

    add product api

--- src/services/ProductApi.jsx
+++ src/services/ProductApi.jsx
@@ -8,3 +8,12 @@ export const loginUser = async (username, password) => {
 
   return response.data;
 };
+
+// Get products
+export const getProducts = async (limit = 10, skip = 0) => {
+  const response = await api.get(
+    `/products?limit=${limit}&skip=${skip}`
+  );
+
+  return response.data;
+};
